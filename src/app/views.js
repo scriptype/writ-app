532b1d91589ceaa47d09b59468e4f976205d2112
scriptype <scriptyper@gmail.com>
2022-12-08 02:22:28 +0200

    Convert forgotten views to use async/await

--- src/app/views.js
+++ src/app/views.js
@@ -2,26 +2,23 @@ import { mockSettings } from './settings.js'
 import Constants from './constants.js'
 import { getRandomFrom, sleep } from './utils.js'
 
-export const IntroView = ({ el }) =>
-  new Promise((resolve, reject) => {
-    el.innerHTML = 'intro view'
-    setTimeout(resolve, 3000)
-  })
+export const IntroView = async ({ el }) => {
+  el.innerHTML = 'intro view'
+  return sleep(3)
+}
 
-export const DefineSettingsView = ({ el }) =>
-  new Promise((resolve, reject) => {
-    el.innerHTML = 'define settings view'
-    setTimeout(() => {
-      resolve({
-        settings: mockSettings,
-        theWayItsDefined: getRandomFrom([
-          Constants.IMPORT_SETTINGS,
-          Constants.CREATE_SETTINGS_USING_FORM,
-          Constants.SOMEHOW_SETTINGS_ARE_THERE
-        ])
-      })
-    }, 3000)
-  })
+export const DefineSettingsView = async ({ el }) => {
+  el.innerHTML = 'define settings view'
+  await sleep(3)
+  return {
+    settings: mockSettings,
+    theWayItsDefined: getRandomFrom([
+      Constants.IMPORT_SETTINGS,
+      Constants.CREATE_SETTINGS_USING_FORM,
+      Constants.SOMEHOW_SETTINGS_ARE_THERE
+    ])
+  }
+}
 
 export const SettingsLoadingView = async ({ el }) => {
   el.innerHTML = 'loading settings'
