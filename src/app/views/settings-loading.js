f9e1248d30c6a8c604403647366dd3394fbc6b46
scriptype <scriptyper@gmail.com>
2022-12-13 07:21:37 +0200

    Add a loading animation

--- src/app/views/settings-loading.js
+++ src/app/views/settings-loading.js
@@ -1,10 +1,22 @@
+const template = () => `
+  <div class="view settings-loading-view">
+    <span class="dummy-logo">Writ</span>
+  </div>
+`
+
 const SettingsLoadingView = async ({ el }) => {
-  const template = () => `
-    <div class="view settings-loading-view">
-      <h1>loading</h1>
-    </form>
-  `
+  const animationDuration = 1500
+
   el.innerHTML = template()
+  el.style.cssText += `;
+    --animation-duration: ${animationDuration}ms;
+  `
+
+  const listenToAnimationEnd = (onEnd) => {
+    setTimeout(onEnd, animationDuration)
+  }
+
+  await new Promise(listenToAnimationEnd)
 }
 
 export default SettingsLoadingView
